0f97a711fa642eb591396b23d7164f8ed2623e4d
Daniel Elsberry (RIT Student) <dwe5866@ad.rit.edu>
2016-04-04 14:43:22 -0400

    Made Goodie Spawner script, played with size increasing

---
+++ Assets/Scripts/GoodieSpawner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoodieSpawner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
