c8ac2225664ad53fec2e0c4230644da1effa0371
Daniel Elsberry (RIT Student) <dwe5866@ad.rit.edu>
2016-04-08 13:32:09 -0400

    Prototype Goodie Spawner script. God help us all.

--- Assets/Scripts/GoodieSpawner.cs
+++ Assets/Scripts/GoodieSpawner.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class GoodieSpawner : MonoBehaviour {
 
-	// Use this for initialization
+    Vector3 spawnPos;
+    GameObject sprinkle;
+    GameObject chocolate;
+    
+    // Use this for initialization
 	void Start () {
+        sprinkle = GameObject.Find("/Sprinkle");
+        chocolate = GameObject.Find("Chocolate Chunk");
+
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        SpawnSprinkle();
 	}
+
+    void SpawnSprinkle()
+    {
+        spawnPos = new Vector3(Random.Range(-5.0f, 5.0f), 0.5f, Random.Range(-5.0f, 5.0f));
+        Instantiate(sprinkle, spawnPos, Quaternion.identity);
+    }
 }
