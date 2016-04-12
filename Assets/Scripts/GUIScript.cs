af81bc503536760d23eda7b25b74388d32ca8d72
Dan Martin <dcm5659@rit.edu>
2016-04-11 22:16:38 -0400

    changed the UI script added some GUI skins

--- Assets/Scripts/GUIScript.cs
+++ Assets/Scripts/GUIScript.cs
@@ -3,14 +3,30 @@ using System.Collections;
 
 public class GUIScript : MonoBehaviour {
 
+    private GUISkin startBtn;
+
+    void Start()
+    {
+        //assigning skin
+        startBtn = Resources.Load("ButtonSkin") as GUISkin;
+    }
+
 	void OnGUI () {
-        Rect rect = new Rect(0, 0, Screen.width / 2, Screen.height / 2);
-        rect.center = new Vector2(Screen.width/2, Screen.height/2);
-		// Make a background box
-		GUI.Box(rect, "Menu Box");
+
+        GUI.skin = startBtn;
+        //declaring rects for buttons
+        Rect menuRect = new Rect(0, 0, Screen.width / 2, Screen.height / 2);
+        Rect startRect = new Rect(0, 0, 200, 100);
+
+        //centering buttons
+        menuRect.center = new Vector2(Screen.width/2, Screen.height/2);
+        startRect.center = new Vector2(Screen.width / 2, Screen.height/3);
+
+        // Make a background box
+        GUI.Box(menuRect, "Menu Box");
 		
-		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-		if(GUI.Button(new Rect(10, 10, 100, 90), "Level 1")) {
+		// Make the start button
+		if(GUI.Button(startRect, "Level 1")) {
 			
 		}
 	}
