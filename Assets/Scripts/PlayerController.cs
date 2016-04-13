91c2458ed34df810abc7bef6951756f360069b1b
Daniel Elsberry (RIT Student) <dwe5866@ad.rit.edu>
2016-04-13 14:47:21 -0400

    Added a way to kill the snowball, end state.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,11 +10,19 @@ public class PlayerController : MonoBehaviour {
 	private float slowMulti;
 	private Rigidbody rb;
 
+    int score;
+
+    bool isAlive;
+
 	void Start()
 	{
 		rb = GetComponent<Rigidbody>();
 		spWeight=0.99f;
 		chWeight=0.95f;
+
+        score = 0;
+
+        isAlive = true;
 	}
 	void FixedUpdate()
     {
@@ -26,6 +34,8 @@ public class PlayerController : MonoBehaviour {
 		rb.AddForce (movement*speed);
 
         transform.localScale -= new Vector3(0.0003f, 0.0003f, 0.0003f);
+
+        SizeCheck();
     }
 	void OnCollisionEnter(Collision pickups)
 	{
@@ -36,16 +46,29 @@ public class PlayerController : MonoBehaviour {
             pickups.transform.parent = transform;
 			if(pickups.collider.tag=="Sprinkle")
 			{
-				//slowMulti=Mathf.Pow(spWeight,transform.childCount);
+                score += 150;
+                
+                //slowMulti=Mathf.Pow(spWeight,transform.childCount);
 				//speed*=slowMulti;
 			}
 			if(pickups.collider.tag=="Chocolate")
 			{
-				//slowMulti=Mathf.Pow(spWeight,transform.childCount);
+                score += 100;
+                
+                //slowMulti=Mathf.Pow(spWeight,transform.childCount);
 				//speed*=slowMulti;
 			}
             //Debug.Log(speed);
             transform.localScale += new Vector3(0.02f, 0.02f, 0.02f); 
 		}
 	}
+
+    void SizeCheck()
+    {
+        if (transform.localScale.x < 0.2f)
+        {
+            isAlive = false;
+            this.gameObject.SetActive(false); 
+        }
+    }
 }
