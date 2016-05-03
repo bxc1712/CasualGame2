b53eb65d49b6c078b03a4d7620762e96460b5bc0
BENJAMIN CHENG (RIT Student) <bxc1712@ad.rit.edu>
2016-05-03 16:39:11 -0400

    Camera should lock to player now, always be behind it

--- Assets/Scripts/CAMController.cs
+++ Assets/Scripts/CAMController.cs
@@ -5,10 +5,12 @@ public class CAMController: MonoBehaviour {
 	public GameObject player;
 	
 	private Vector3 offset;
-	
+	private GameObject target;
 	// Use this for initialization
 	void Start () {
 		offset = transform.position - player.transform.position;
+		target=GameObject.Find("Snowball");
+		transform.LookAt(target.transform);
 	}
 	
 	void LateUpdate () {
