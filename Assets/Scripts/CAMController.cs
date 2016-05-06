1245c7447f501851f08a926d3378a62c7c17252e
BENJAMIN CHENG (RIT Student) <bxc1712@ad.rit.edu>
2016-05-06 13:29:14 -0400

    Camera Fixes

--- Assets/Scripts/CAMController.cs
+++ Assets/Scripts/CAMController.cs
@@ -2,19 +2,46 @@
 using System.Collections;
 
 public class CAMController: MonoBehaviour {
-	public GameObject player;
-	
 	private Vector3 offset;
-	private GameObject target;
-	// Use this for initialization
+	public GameObject target;
+	//public float rotateSpeed = 5;
+	private float mouseX;
+	private float mouseY;
+
 	void Start () {
-		offset = transform.position - player.transform.position;
-		target=GameObject.Find("Snowball");
-		transform.LookAt(target.transform);
+		offset = transform.position - target.transform.position;
 	}
-	
 	void LateUpdate () {
-		transform.position = player.transform.position + offset;
-		Debug.Log (offset);
+		camerRotate();
+
+
+		Vector3 desiredPosition = target.transform.position + offset;
+		transform.position=desiredPosition;
+//		transform.position = desiredPosition;
+//		float horizontal = Input.GetAxis ("Mouse X") * rotateSpeed;
+//		target.transform.Rotate (0, horizontal, 0);
+//
+//		float desiredAngle = target.transform.eulerAngles.y;
+//		Quaternion rotation = Quaternion.Euler (0, desiredAngle, 0);
+//		transform.position = target.transform.position - (rotation * offset);
+//		transform.LookAt(target.transform);
+//		transform.position = player.transform.position + offset;
+
+		mouseX = Input.mousePosition.x;
+		mouseY = Input.mousePosition.y;
+	}
+
+	void camerRotate(){
+		var ease = 10f;
+		if (Input.GetMouseButton (0)) {
+			if (Input.mousePosition.x != mouseX) {
+				var cameraRotationY = (mouseX-Input.mousePosition.x) * ease * Time.deltaTime;
+				this.transform.Rotate (0, cameraRotationY, 0);
+			}
+			if(Input.mousePosition.y != mouseY){
+				var cameraRotationX = (Input.mousePosition.y-mouseY) * ease * Time.deltaTime;
+				this.transform.Rotate (cameraRotationX, 0, 0);
+			}
+		}
 	}
 }
