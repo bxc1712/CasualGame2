f0d9854704ea07d942dc6e1d4aa2032174a76281
Dan Martin <dcm5659@rit.edu>
2016-05-06 11:14:54 -0400

    Made a pause function reformatted code

--- Assets/Scripts/MM.cs
+++ Assets/Scripts/MM.cs
@@ -5,29 +5,21 @@ public class MM : MonoBehaviour {
     private bool paused;
     private bool showGUI;
 
+    string levelName;
 
     void Start()
     {
         paused = false;
         showGUI = false;
+        levelName = Application.loadedLevelName;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown("p"))
+        if (Input.GetKeyDown("p") && levelName == "TestScene")
         {
-            paused = !paused;
-            showGUI = !showGUI;
-        }
-
-        if(paused == true)
-        {
-            Time.timeScale = 0;
-        }
-        else
-        {
-            Time.timeScale = 1;
-        }
+            pauseGame();
+        }   
     }
 
     public void startGame(string changeTo)
@@ -38,6 +30,21 @@ public class MM : MonoBehaviour {
     public void loadInstructions()
     {
 
+    }
+
+    void pauseGame()
+    {
+        paused = !paused;
+        showGUI = !showGUI;
+        Debug.Log("paused");
+        if (paused == true)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
     }
 
 }
