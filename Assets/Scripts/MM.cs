df4dcd52d3bd36140d60c85c26300860d20a0dfc
Dan Martin <dcm5659@rit.edu>
2016-05-18 23:16:45 -0400

    Added images to UI Fixed some bugs

--- Assets/Scripts/MM.cs
+++ Assets/Scripts/MM.cs
@@ -8,7 +8,7 @@ public class MM : MonoBehaviour {
 
     string levelName;
 
-    public GameObject pMenu;
+    private GameObject pMenu;
     void Start()
     {
         paused = false;
@@ -22,7 +22,7 @@ public class MM : MonoBehaviour {
 
         catch
         {
-
+            showGUI = false;
         }    
     }
 
